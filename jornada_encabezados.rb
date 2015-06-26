6927d2f6286f5c9defd5d719d2d19e533d2c01ee
thotmx <hermes.o.r@gmail.com>
2015-06-26 18:33:35 -0500

    Simple formatting for the output

--- jornada_encabezados.rb
+++ jornada_encabezados.rb
@@ -5,8 +5,11 @@ require 'mechanize'
 agent = Mechanize.new
 
 ((Date.today - 30)..(Date.today)).each do |date|
+  puts "*"*50
+  puts date
   page = agent.get "http://www.jornada.unam.mx/#{date.year}/#{"%02d" % date.month}/#{"%02d" % date.day}"
   @headers = page.root.css('.cabeza > a')
-  puts @headers.map{ |header| { text: header.text, link: header['href'] } }
+  puts @headers.map(&:text)
+  puts "*"*50
 end
 

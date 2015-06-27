64732d12a6afc8da1d492c398417d1761df7cb3e
thotmx <hermes.o.r@gmail.com>
2015-06-26 19:14:11 -0500

    Simple format for rayuelas

--- rayuela.rb
+++ rayuela.rb
@@ -4,8 +4,11 @@ require 'watir-webdriver'
 
 browser = Watir::Browser.new
 ((Date.today - 30)..(Date.today)).each do |date|
+  puts "*"*50
+  puts date
   browser.goto "http://www.jornada.unam.mx/#{date.year}/#{"%02d" % date.month}/#{"%02d" % date.day}"
   Watir::Wait.until{ browser.span(id: 'rayuela-text').text }
   rayuela = browser.span(id: 'rayuela-text')
-  puts rayuela.text
+  puts "\"#{rayuela.text}\""
+  puts "*"*50
 end

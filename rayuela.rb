f2d998ad407594f40d4ae2e3e69e267bed10444f
thotmx <hermes.o.r@gmail.com>
2015-06-26 19:04:31 -0500

    First simple approach with watir

--- rayuela.rb
+++ rayuela.rb
@@ -3,3 +3,7 @@ require 'bundler/setup'
 require 'watir-webdriver'
 
 browser = Watir::Browser.new
+browser.goto "http://www.jornada.unam.mx/2015/06/25"
+rayuela = browser.span(id: 'rayuela-text')
+
+puts rayuela.text

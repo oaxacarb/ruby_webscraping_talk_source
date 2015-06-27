246d1b692404bb3d52f076a925217b49f3e06d8a
thotmx <hermes.o.r@gmail.com>
2015-06-26 19:19:02 -0500

    Adding headless support

--- rayuela.rb
+++ rayuela.rb
@@ -1,9 +1,12 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'watir-webdriver'
+require 'headless'
 
+headless = Headless.new
+headless.start
 browser = Watir::Browser.new
-((Date.today - 30)..(Date.today)).each do |date|
+((Date.today - 7)..(Date.today)).each do |date|
   puts "*"*50
   puts date
   browser.goto "http://www.jornada.unam.mx/#{date.year}/#{"%02d" % date.month}/#{"%02d" % date.day}"
@@ -12,3 +15,6 @@ browser = Watir::Browser.new
   puts "\"#{rayuela.text}\""
   puts "*"*50
 end
+
+browser.close
+headless.destroy
